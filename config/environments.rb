7dc0a35c509815dd2557fa16e116966f79d1b16e
Shane Boyar <shane.boyar@gmail.com>
2017-05-13 23:58:02 -0400

    oh god

--- config/environments.rb
+++ config/environments.rb
@@ -1,14 +1,14 @@
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
 configure :production, :development do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  # db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/production')
 
   ActiveRecord::Base.establish_connection(
-      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-      :host     => db.host,
-      :username => db.user,
-      :password => db.password,
-      :database => db.path[1..-1],
+      :adapter => 'postgresql',
+      :host     => 'localhost',
+      :username => 'shaneboyar',
+      :password => 'Humb3rthumb3rt',
+      :database => 'development',
       :encoding => 'utf8'
   )
 end
